add61fce53704d76d373493c0b581ae60f2642ac
alex <alexkudinov99@gmail.com>
2026-08-20 16:32:15 +0200

    Initialize Spring Boot project

---
+++ src/main/java/boulderino/boulderino/BoulderinoApplication.java
@@ -0,0 +1,13 @@
+package boulderino.boulderino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoulderinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulderinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boulderino/boulderino/BoulderinoApplicationTests.java
@@ -0,0 +1,13 @@
+package boulderino.boulderino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulderinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
